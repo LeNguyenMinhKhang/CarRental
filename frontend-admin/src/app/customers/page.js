6b4c33f94b0c2fb0e6627327f59677cb1444833d
Khang <23520690@gm.uit.edu.vn>
2025-03-19 17:10:20 +0700

    customer

--- frontend-admin/src/app/customers/page.js
+++ frontend-admin/src/app/customers/page.js
@@ -7,7 +7,7 @@ const CustomersPage = () => {
     return (
       <div className="flex flex-row w-full h-screen bg-[#F4F7FE]">
         <Sidebar />
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-6 ml-55">
           <CustomerList />
         </main>
       </div>
